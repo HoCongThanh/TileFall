04c17f13edf72cb182df8655ea38ed5a32330633
Hồ Công Thành <hothanh@wolffungame.com>
2022-05-16 20:00:24 +0700

    Fix bug camera dont follow player

--- quantum_unity/Assets/Script/PlayerEntityView.cs
+++ quantum_unity/Assets/Script/PlayerEntityView.cs
@@ -1,7 +1,9 @@
 ﻿using Quantum;
 using Quantum.Demo;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerEntityView : MonoBehaviour
@@ -14,15 +16,29 @@ public class PlayerEntityView : MonoBehaviour
     [SerializeField]
     private TextMesh txtMesh;
 
-    private void Start()
+    bool Init = false;
+
+    private unsafe void Awake()
     {
-        InitEntityView(QuantumRunner.Default.Game.Frames.Verified);
+        StartCoroutine(SetUpPlayerEntity());
+    }
+
+    private IEnumerator SetUpPlayerEntity()
+    {
+        yield return new WaitUntil(() => QuantumRunner.Default.Session != null && QuantumRunner.Default.Session.FrameVerified != null);
+        var f = QuantumRunner.Default.Game.Frames.Verified;
+        yield return new WaitUntil(() => f.Exists(entityView.EntityRef));
+
+        Debug.Log("SetUpPlayerEntity");
+        InitEntityView(f);
     }
 
     private unsafe void InitEntityView(Frame f)
     {
         if(f.Unsafe.TryGetPointer<PlayerComp>(entityView.EntityRef, out var playerComp))
         {
+            Debug.Log("PlayerEntityView Start " + playerComp->playerRef._index + "_" + QuantumRunner.Default.Session.IsLocalPlayer(playerComp->playerRef));
+
             if (QuantumRunner.Default.Session.IsLocalPlayer(playerComp->playerRef))
             {
                 LocalPlayerEntityView = this;
